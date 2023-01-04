64d3a254092d5789c60eec41e9a9544536a633c8
Douglas Vieira <douglasdotv@hotmail.com>
2023-01-04 04:32:39 -0300

    Update DoctorController to implement DTO pattern

--- src/main/java/br/com/dv/api/controller/DoctorController.java
+++ src/main/java/br/com/dv/api/controller/DoctorController.java
@@ -1,5 +1,6 @@
 package br.com.dv.api.controller;
 
+import br.com.dv.api.doctor.DoctorRegistrationData;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DoctorController {
 
     @PostMapping
-    public void register(@RequestBody String json) {
-        System.out.println(json);
+    public void register(@RequestBody DoctorRegistrationData doctorRegistrationData) {
+        System.out.println(doctorRegistrationData);
     }
 
 }
