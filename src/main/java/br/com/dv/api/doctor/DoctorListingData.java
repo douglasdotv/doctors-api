4c70118f857788e8a62840c2ee580fac86ececce
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-09 03:04:04 -0300

    Refactor DoctorListing DTO

--- src/main/java/br/com/dv/api/doctor/DoctorListingData.java
+++ src/main/java/br/com/dv/api/doctor/DoctorListingData.java
@@ -1,6 +1,10 @@
 package br.com.dv.api.doctor;
 
-public record DoctorListingData(Long id, String name, String email, String crm, Specialty specialty) {
+public record DoctorListingData(Long id,
+                                String name,
+                                String email,
+                                String crm,
+                                Specialty specialty) {
 
     public DoctorListingData(Doctor doctor) {
         this(doctor.getId(), doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getSpecialty());
