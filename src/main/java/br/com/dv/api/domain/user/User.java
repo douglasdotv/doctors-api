90c86b0ed9404b8392078b61081610dd74427e7e
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-10 18:22:03 -0300

    Create users table (migration) and User entity

---
+++ src/main/java/br/com/dv/api/domain/user/User.java
@@ -0,0 +1,23 @@
+package br.com.dv.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+
+}
---
+++ src/main/resources/db/migration/V5__create-table-users.sql
@@ -0,0 +1,10 @@
+# note: login must be unique, and password must be encrypted
+
+create table users
+(
+    id       bigint       not null auto_increment,
+    login    varchar(100) not null,
+    password varchar(255) not null,
+
+    primary key (id)
+);
