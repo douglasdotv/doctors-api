9f89520bc0b7698cc9a2093d1d146a32d27f1100
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-10 19:24:35 -0300

    Create UserRepository and AuthenticationService

---
+++ src/main/java/br/com/dv/api/domain/user/AuthenticationService.java
@@ -0,0 +1,24 @@
+package br.com.dv.api.domain.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+
+}
