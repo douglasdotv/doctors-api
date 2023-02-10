548882cfb948b870d77184bd3ecf1f80975ae5f3
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-10 01:18:43 -0300

    Add global error handling for REST controllers

---
+++ src/main/java/br/com/dv/api/infra/ErrorHandler.java
@@ -0,0 +1,37 @@
+package br.com.dv.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    // We should return 404 instead of 500 when the entity is not found
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> handleEntityNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    // We should return 400 and a json with the errors when there are validation errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorData>> handleMethodArgumentNotValidExcpetion(MethodArgumentNotValidException
+                                                                                             exception) {
+        var errors = exception.getBindingResult().getFieldErrors();
+        var errorData = errors.stream().map(ErrorData::new).toList();
+
+        return ResponseEntity.badRequest().body(errorData);
+    }
+
+    private record ErrorData(String field, String message) {
+        public ErrorData(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+
+}
