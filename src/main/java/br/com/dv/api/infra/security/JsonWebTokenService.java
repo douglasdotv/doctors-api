c215d675d2cb1ab42b43f2fe76b3c6010090d43f
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-11 05:45:01 -0300

    Enhance jwt service to use secret from a config file

--- src/main/java/br/com/dv/api/infra/security/JsonWebTokenService.java
+++ src/main/java/br/com/dv/api/infra/security/JsonWebTokenService.java
@@ -4,6 +4,7 @@ import br.com.dv.api.domain.user.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,9 +14,12 @@ import java.util.Date;
 @Service
 public class JsonWebTokenService {
 
+    @Value("${jwt.secret}")
+    private String secret;
+
     public String generateToken(User user) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("123456");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("REST API Issuer")
                     .withSubject(user.getUsername())
