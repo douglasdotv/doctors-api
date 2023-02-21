ce92cd1dee4aa240e8a3b49645f6a1e95631d9de
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-21 16:37:24 -0300

    Add validation: check doctor id/patient id

--- src/main/java/br/com/dv/api/domain/appointment/AppointmentScheduler.java
+++ src/main/java/br/com/dv/api/domain/appointment/AppointmentScheduler.java
@@ -1,7 +1,10 @@
 package br.com.dv.api.domain.appointment;
 
+import br.com.dv.api.domain.appointment.validation.AppointmentValidationException;
+import br.com.dv.api.domain.doctor.Doctor;
 import br.com.dv.api.domain.doctor.DoctorRepository;
 import br.com.dv.api.domain.patient.PatientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,10 +25,24 @@ public class AppointmentScheduler {
     }
 
     public void schedule(AppointmentSchedulingDto dto) {
-        var doctor = doctorRepository.findById(dto.doctorId()).get();
-        var patient = patientRepository.findById(dto.patientId()).get();
+        boolean doctorIdIsPresent = dto.doctorId() != null;
+
+        var patient = patientRepository.findById(dto.patientId())
+                .orElseThrow(() -> new AppointmentValidationException("Patient id not found"));
+
+        var doctor = doctorIdIsPresent ?
+                doctorRepository.findById(dto.doctorId())
+                        .orElseThrow(() -> new AppointmentValidationException("Doctor id not found")) :
+                chooseDoctor(dto);
+
         var appointment = new Appointment(null, doctor, patient, dto.date(), dto.specialty());
+
         appointmentRepository.save(appointment);
+    }
+
+    private Doctor chooseDoctor(AppointmentSchedulingDto dto) {
+        // TODO
+        return null;
     }
 
 }
---
+++ src/main/java/br/com/dv/api/domain/appointment/validation/AppointmentValidationException.java
@@ -0,0 +1,9 @@
+package br.com.dv.api.domain.appointment.validation;
+
+public class AppointmentValidationException extends RuntimeException {
+
+    public AppointmentValidationException(String message) {
+        super(message);
+    }
+
+}
