b572ce2ff54c3e56d8bf45e44978dc39bc4b46c0
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-22 20:10:34 -0300

    Add validation: check scheduling time

---
+++ src/main/java/br/com/dv/api/domain/appointment/validation/AdvanceNoticeSchedulingValidation.java
@@ -0,0 +1,26 @@
+package br.com.dv.api.domain.appointment.validation;
+
+import br.com.dv.api.domain.appointment.AppointmentSchedulingDto;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class AdvanceNoticeSchedulingValidation {
+
+    private static final int MINUTES_IN_ADVANCE = 30;
+
+    public void validate(AppointmentSchedulingDto dto) {
+        /*
+        Appointments must be scheduled at least 30 minutes in advance.
+         */
+
+        var appointmentDateTime = dto.scheduledDateTime();
+        var now = LocalDateTime.now();
+        var diff = Duration.between(now, appointmentDateTime).toMinutes();
+
+        if (diff < MINUTES_IN_ADVANCE) {
+            throw new AppointmentValidationException("Appointments must be scheduled at least 30 minutes in advance.");
+        }
+    }
+
+}
