29c697c9c8675a74f29d0a1d808a721e1b96a8e9
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-22 18:27:31 -0300

    Add validation: check opening hours

---
+++ src/main/java/br/com/dv/api/domain/appointment/validation/OpeningHoursValidation.java
@@ -0,0 +1,31 @@
+package br.com.dv.api.domain.appointment.validation;
+
+import br.com.dv.api.domain.appointment.AppointmentSchedulingDto;
+import java.time.DayOfWeek;
+
+public class OpeningHoursValidation {
+
+    private static final int OPENING_HOUR = 7;
+    private static final int CLOSING_HOUR = 19;
+    private static final int APPOINTMENT_DURATION = 1;
+
+    public void validate(AppointmentSchedulingDto dto) {
+        /*
+        - Can't schedule appointments on Sundays
+        - The opening hours are from 7am to 7pm
+        - The appointment duration is fixed to 1 hour
+         */
+
+        var appointmentDateTime = dto.scheduledDateTime();
+        var appointmentDayOfWeek = appointmentDateTime.getDayOfWeek();
+
+        var isSunday = appointmentDayOfWeek == DayOfWeek.SUNDAY;
+        var isbeforeOpeningHours = appointmentDateTime.getHour() < OPENING_HOUR;
+        var isAfterClosingHours = appointmentDateTime.getHour() > CLOSING_HOUR - APPOINTMENT_DURATION;
+
+        if (isSunday || isbeforeOpeningHours || isAfterClosingHours) {
+            throw new AppointmentValidationException("Appointment cannot be scheduled outside opening hours");
+        }
+
+    }
+}
